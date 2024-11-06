1144ab736143a119c14fc8b387deec6568cadd84
mangsuyo <mangsuyo3610@gmail.com>
2024-11-07 03:15:36 +0900

    feat: Svg 아이콘 변환 기능 (#4)

--- src/screens/onboarding/UniversitySelectScreen.tsx
+++ src/screens/onboarding/UniversitySelectScreen.tsx
@@ -1,10 +1,11 @@
 import Layout from "@/components/common/Layout";
 import React from "react";
 import { View, Text } from "react-native";
-
+import SejongLogo from "@assets/icons/universities/sejong.svg";
 export default function UniversitySelectScreen() {
   return (
     <Layout>
+      <SejongLogo width={24} height={24} />
       <Text>UniversitySelectScreen</Text>
     </Layout>
   );
