ca2eb8d9e0160258c8da923c4b919871bcf2fe06
mangsuyo <mangsuyo3610@gmail.com>
2025-01-01 18:17:36 +0900

    refactor: Splash 스크린 메서드 분리

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,54 +1,51 @@
 import React, { useEffect } from "react";
-import { View, ActivityIndicator, Image } from "react-native";
+import { View, Image } from "react-native";
 import * as Font from "expo-font";
-import { getAccessToken, removeTokens } from "@/utils/service/auth";
+import { getAccessToken } from "@/utils/service/auth";
+
+const FONTS = {
+  "Pretendard-Thin": require("@assets/fonts/Pretendard-Thin.otf"),
+  "Pretendard-ExtraLight": require("@assets/fonts/Pretendard-ExtraLight.otf"),
+  "Pretendard-Light": require("@assets/fonts/Pretendard-Light.otf"),
+  "Pretendard-Regular": require("@assets/fonts/Pretendard-Regular.otf"),
+  "Pretendard-Medium": require("@assets/fonts/Pretendard-Medium.otf"),
+  "Pretendard-SemiBold": require("@assets/fonts/Pretendard-SemiBold.otf"),
+  "Pretendard-Bold": require("@assets/fonts/Pretendard-Bold.otf"),
+  "Pretendard-ExtraBold": require("@assets/fonts/Pretendard-ExtraBold.otf"),
+  "Pretendard-Black": require("@assets/fonts/Pretendard-Black.otf"),
+};
 
 export default function SplashScreen({ navigation }) {
   useEffect(() => {
     initializeApp();
   }, []);
 
-  const initializeApp = async () => {
-    try {
-      const [accessToken] = await Promise.all([
-        getAccessToken(),
-        loadFonts(),
-        new Promise((resolve) => setTimeout(resolve, 1000)),
-      ]);
-      if (accessToken) {
-        navigation.navigate("Tab");
-      } else {
-        navigation.navigate("Onboarding", {
-          screen: "OnboardingWelcome",
-        });
-      }
-    } catch (error) {
-      console.error("App initialization failed:", error);
-    }
+  const loadFonts = async () => {
+    await Font.loadAsync(FONTS);
   };
 
-  const loadFonts = async () => {
+  const handleNavigation = async () => {
+    const accessToken = await getAccessToken();
+    navigation.navigate(accessToken ? "Tab" : "Onboarding");
+  };
+
+  const initializeApp = async () => {
     try {
-      await Font.loadAsync({
-        "Pretendard-Thin": require("@assets/fonts/Pretendard-Thin.otf"),
-        "Pretendard-ExtraLight": require("@assets/fonts/Pretendard-ExtraLight.otf"),
-        "Pretendard-Light": require("@assets/fonts/Pretendard-Light.otf"),
-        "Pretendard-Regular": require("@assets/fonts/Pretendard-Regular.otf"),
-        "Pretendard-Medium": require("@assets/fonts/Pretendard-Medium.otf"),
-        "Pretendard-SemiBold": require("@assets/fonts/Pretendard-SemiBold.otf"),
-        "Pretendard-Bold": require("@assets/fonts/Pretendard-Bold.otf"),
-        "Pretendard-ExtraBold": require("@assets/fonts/Pretendard-ExtraBold.otf"),
-        "Pretendard-Black": require("@assets/fonts/Pretendard-Black.otf"),
-      });
+      await loadFonts();
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      await handleNavigation();
     } catch (error) {
-      console.error("Font loading failed:", error);
+      console.error("[앱 초기화 실패]:", error);
+      navigation.navigate("Onboarding");
     }
   };
 
   return (
-    <View className="flex-1 justify-center items-center bg-[#00A176]">
-      <Image source={require("@assets/splash.png")} width={203} height={206} />
-      <ActivityIndicator size="large" color="#007AFF" />
+    <View className="flex-1 justify-center items-center">
+      <Image
+        source={require("@assets/images/splash.png")}
+        className="w-full h-full"
+      />
     </View>
   );
 }
