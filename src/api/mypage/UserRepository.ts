6720163a0bb8be635086f6ffc72b9ca705ba2671
박현우 <gusdn3477@naver.com>
2025-01-04 15:12:13 +0900

    feat: 로그인 이후 사용되는 유저 관련 api를 담은 UserRepository 구현

---
+++ src/api/mypage/UserRepository.ts
@@ -0,0 +1,33 @@
+import apiClient from "../apiClient";
+
+class UserRepository {
+
+    async get() {
+        const { data } = await apiClient.get(`/mypage`);
+        return data;
+    };
+
+    async updateName(name: string) {
+        const { data } = await apiClient.patch(`/mypage/update/name`, { name });
+        return data;
+    };
+
+    async updateLanguages(languages: string[]) {
+        const { data } = await apiClient.patch(`/mypage/update/languages`, { languages });
+        return data;
+    };
+
+    async updateInterests(interests: string[]) {
+        const { data } = await apiClient.patch(`/mypage/update/interests`, { interests });
+        return data;
+    };
+
+    async updateProfileImage(imageKey: string) {
+        const { data } = await apiClient.patch(
+            `/mypage/update/profile-default-image?profileImageKey=${imageKey}`
+        );
+        return data;
+    };
+}
+
+export default new UserRepository();
