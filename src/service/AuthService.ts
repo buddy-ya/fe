6a79eedd4768f78b3549798b0ea0b74a0f4bcf30
박현우 <gusdn3477@naver.com>
2025-01-04 17:48:39 +0900

    feat: 토큰 관련 서비스 관리하는 AuthService 클래스 구현

---
+++ src/service/AuthService.ts
@@ -0,0 +1,64 @@
+import { TOKEN } from "@/utils/constants/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+class AuthService {
+
+  private accessToken: string | null;
+  private refreshToken: string | null;
+
+  async saveTokens(
+    accessToken: string,
+    refreshToken: string | null
+  ) {
+    try {
+      await AsyncStorage.setItem(TOKEN.ACCESS_TOKEN, accessToken);
+      refreshToken && await AsyncStorage.setItem(TOKEN.REFRESH_TOKEN, refreshToken);
+    } catch (error) {
+      console.error("Error saving tokens:", error);
+    }
+  };
+
+  async getAccessToken() {
+    try {
+      this.accessToken ??= await AsyncStorage.getItem(TOKEN.ACCESS_TOKEN);
+      return this.accessToken;
+    } catch (error) {
+      console.error("Error getting access token:", error);
+      return null;
+    }
+  };
+
+  async getRefreshToken() {
+    try {
+      this.refreshToken ??= await AsyncStorage.getItem(TOKEN.REFRESH_TOKEN)
+      return this.refreshToken;
+    } catch (error) {
+      console.error("Error getting refresh token:", error);
+      return null;
+    }
+  };
+
+  async removeTokens() {
+    try {
+      await AsyncStorage.removeItem(TOKEN.ACCESS_TOKEN);
+      await AsyncStorage.removeItem(TOKEN.REFRESH_TOKEN);
+      this.accessToken = null;
+      this.refreshToken = null;
+    } catch (error) {
+      console.error("Error removing tokens:", error);
+    }
+  };
+
+  async isTokenPresent() {
+    try {
+      const accessToken = await this.getAccessToken();
+      const refreshToken = await this.getRefreshToken();
+      return !!(accessToken && refreshToken);
+    } catch (error) {
+      console.error("Error checking tokens:", error);
+      return false;
+    }
+  };
+}
+
+export default new AuthService();
