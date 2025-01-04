36a77ffad292041f3f1b7bd1f7dac882991cbd82
박현우 <gusdn3477@naver.com>
2025-01-04 22:59:59 +0900

    feat: Onboarding Repo &  DTO 작성

---
+++ src/types/OnboardingDTO.ts
@@ -0,0 +1,14 @@
+type Gender = "male" | "female" | "unknown";
+
+export interface createInfoDTO {
+    name: string;
+    country: string;
+    isKorean: boolean;
+    isNotificationEnabled: boolean;
+    phoneNumber: string;
+    gender: Gender;
+    university: string;
+    majors: string[];
+    languages: string[];
+    interests: string[];
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * as Auth from './AuthDTO';
 export * as Comment from './CommentDTO';
 export * as Feed from './FeedDTO';
+export * as Onboarding from './OnboardingDTO';
