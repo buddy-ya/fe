0d26a58b9dfdaafcf2a70a8658cd69a2735de0d1
박현우 <gusdn3477@naver.com>
2025-01-05 21:35:20 +0900

    feat: 토큰 셋팅해주는 AuthProvider 구현 및 App.tsx 감싸기 작업

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
-import React, { Suspense, useEffect } from "react";
+import { Suspense } from "react";
 import { I18nextProvider } from "react-i18next";
 import i18n from "@/i18n";
 import Router from "@navigation/router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "@/AuthProvider";
 
 export default function App() {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<></>}>
-        <I18nextProvider i18n={i18n}>
-          <GestureHandlerRootView>
-            <Router/>
-          </GestureHandlerRootView>
-        </I18nextProvider>
-      </Suspense>
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Suspense fallback={<></>}>
+          <I18nextProvider i18n={i18n}>
+            <GestureHandlerRootView>
+              <Router />
+            </GestureHandlerRootView>
+          </I18nextProvider>
+        </Suspense>
+      </QueryClientProvider>
+    </AuthProvider>
   );
 }
---
+++ src/AuthProvider.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { getAccessToken } from "./utils/service/auth";
+import { API } from "./api";
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export default function AuthProvider({ children }: Props) {
+
+    const [loading, setLoading] = useState(false);
+
+    const initAuth = async () => {
+        setLoading(true);
+        const accessToken = await getAccessToken();
+        if (accessToken) {
+            API.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        initAuth();
+    }, [])
+
+    if (loading) return <></>;
+    return <>{children}</>
+}
