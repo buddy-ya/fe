bfec8cabcc9dc5ae102ae61bb7ae63cd6d956618
박현우 <gusdn3477@naver.com>
2025-01-07 23:18:55 +0900

    feat: ErrorBoundary, Suspense로 컴포넌트 감싸기

--- App.tsx
+++ App.tsx
@@ -5,20 +5,27 @@ import Router from "@navigation/router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "@/AuthProvider";
+import { ErrorBoundary } from "react-error-boundary";
+import Skeleton from "@/screens/Skeleton";
+import ErrorPage from "@/screens/ErrorPage";
 
 export default function App() {
+
   const queryClient = new QueryClient();
+
   return (
-    <AuthProvider>
-      <QueryClientProvider client={queryClient}>
-        <Suspense fallback={<></>}>
-          <I18nextProvider i18n={i18n}>
-            <GestureHandlerRootView>
-              <Router />
-            </GestureHandlerRootView>
-          </I18nextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary FallbackComponent={ErrorPage}>
+        <Suspense fallback={<Skeleton />}>
+          <AuthProvider>
+            <I18nextProvider i18n={i18n}>
+              <GestureHandlerRootView>
+                <Router />
+              </GestureHandlerRootView>
+            </I18nextProvider>
+          </AuthProvider>
         </Suspense>
-      </QueryClientProvider>
-    </AuthProvider>
+      </ErrorBoundary>
+    </QueryClientProvider >
   );
 }
