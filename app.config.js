1bdd5a3e7b1b732e77b47749f2ccca6857e37efe
mangsuyo <mangsuyo3610@gmail.com>
2025-01-09 22:59:07 +0900

    chore: 앱 icon 변경 (#23)

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ module.exports = {
       resizeMode: 'contain',
       backgroundColor: '#ffffff',
     },
-    icon: './assets/images/logo.png',
+    icon: './assets/images/icon.png',
     plugins: [
       [
         'expo-notifications',
