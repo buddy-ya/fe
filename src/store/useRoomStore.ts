bc2a85b3f9af7a80d45777299fd9ce285b9fde5c
박현우 <gusdn3477@naver.com>
2025-01-14 21:50:19 +0900

    feat: room Store 초기 구현

---
+++ src/store/useRoomStore.ts
@@ -0,0 +1,25 @@
+import { create } from 'zustand';
+import { Room } from '@/model';
+
+
+// Zustand 스토어 타입 정의
+interface State extends Room {
+
+}
+
+interface Action {
+    setRoom: (room: Room) => void;
+}
+
+// Zustand 스토어 생성
+export const useMessageStore = create<State & Action>((set) => ({
+    id: -1,
+    title: '',
+    content: '',
+    imageUrl: '',
+    unreadCount: 0,
+    lastMessageDate: '',
+
+    setRoom: (room: Room) => set({ id: room.id, title: room.title, content: room.content, imageUrl: room.imageUrl, unreadCount: room.unreadCount, lastMessageDate: room.lastMessageDate })
+}));
+
