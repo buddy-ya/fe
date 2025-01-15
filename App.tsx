d9de25518c5f2420d572c2e73f2f652ff764b97f
박현우 <gusdn3477@naver.com>
2025-01-15 20:03:00 +0900

    feat: ErrorBoundary에서 국제화 기능 사용하기 위해 I18n Provider 안쪽으로 이동

--- App.tsx
+++ App.tsx
@@ -7,30 +7,25 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "@/provider";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorPage from "@/screens/ErrorPage";
-import { useBottomModalStore } from "@/store";
-import { BottomModal } from "@/components";
 
 export default function App() {
 
   const queryClient = new QueryClient();
-  const bottomModalVisible = useBottomModalStore(state => state.visible);
-  const bottomModalOptions = useBottomModalStore(state => state.optionList);
-  const handleBottomModalClose = useBottomModalStore(state => state.handleClose);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ErrorBoundary FallbackComponent={ErrorPage}>
-        <Suspense fallback={<></>}>
-          <AuthProvider>
-            <I18nextProvider i18n={i18n}>
-              <GestureHandlerRootView>
+      <I18nextProvider i18n={i18n}>
+        <ErrorBoundary FallbackComponent={ErrorPage}>
+          <Suspense fallback={<></>}>
+            <GestureHandlerRootView>
+              <AuthProvider>
                 <Router />
-                <BottomModal visible={bottomModalVisible} onClose={handleBottomModalClose} options={bottomModalOptions} />
-              </GestureHandlerRootView>
-            </I18nextProvider>
-          </AuthProvider>
-        </Suspense>
-      </ErrorBoundary>
+              </AuthProvider>
+            </GestureHandlerRootView>
+          </Suspense>
+        </ErrorBoundary>
+      </I18nextProvider>
+
     </QueryClientProvider >
   );
 }
