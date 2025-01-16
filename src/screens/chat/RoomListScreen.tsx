2702fbfa39b310e6a84c0a7fde2519a6edf4b3a4
박현우 <gusdn3477@naver.com>
2025-01-16 17:51:44 +0900

    fix: fragment에 key 적용하지 않아 룸리스트 렌더링 시 에러 뜨는 현상 해결

--- src/screens/chat/RoomListScreen.tsx
+++ src/screens/chat/RoomListScreen.tsx
@@ -1,5 +1,5 @@
 import { NavigationProp } from '@react-navigation/native';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import { View, Text } from 'react-native';
 import { ChatPaper, InnerLayout, Layout, RoomList } from '@/components';
 import { Room } from '@/model';
@@ -17,108 +17,109 @@ export const CATEGORIES = [
   },
 ]
 
+
+const rooms: Room[] = [
+  {
+    id: 1,
+    title: "방1",
+    content: "hello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 123,
+    lastMessageDate: "2022-10-10"
+  },
+  {
+    id: 2,
+    title: "방2",
+    content: "hello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 3,
+    lastMessageDate: "2022-10-10"
+  },
+  {
+    id: 3,
+    title: "방3",
+    content: "hello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 0,
+    lastMessageDate: "2022-10-10"
+  },
+  {
+    id: 4,
+    title: "방4",
+    content: "hello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 3,
+    lastMessageDate: "2022-10-10"
+  },
+  {
+    id: 5,
+    title: "방5",
+    content: "hello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 9999,
+    lastMessageDate: "2022-10-10"
+  },
+  {
+    id: 6,
+    title: "방6",
+    content: "hello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 9999,
+    lastMessageDate: "2022-10-10"
+  },
+  {
+    id: 7,
+    title: "방7",
+    content: "hello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 123,
+    lastMessageDate: "2022-10-10"
+  },
+  {
+    id: 8,
+    title: "방8",
+    content: "hello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 3,
+    lastMessageDate: "2022-10-10"
+  },
+  {
+    id: 9,
+    title: "방9",
+    content: "hello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 0,
+    lastMessageDate: "2022-10-10"
+  },
+  {
+    id: 10,
+    title: "방10",
+    content: "hello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 3,
+    lastMessageDate: "2022-10-10"
+  },
+  {
+    id: 11,
+    title: "방11",
+    content: "hello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 9999,
+    lastMessageDate: "2022-10-10"
+  },
+  {
+    id: 12,
+    title: "방12",
+    content: "hello world",
+    imageUrl: "https://via.placeholder.com/150",
+    unreadCount: 9999,
+    lastMessageDate: "2022-10-10"
+  }
+]
+
 export default function RoomListScreen({ navigation }: { navigation: NavigationProp<any> }) {
   const [activeIndex, setActiveIndex] = useState(0);
   const setRoom = useRoomStore(state => state.setRoom);
-
-  const rooms: Room[] = [
-    {
-      id: 1,
-      title: "방1",
-      content: "hello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 123,
-      lastMessageDate: "2022-10-10"
-    },
-    {
-      id: 2,
-      title: "방2",
-      content: "hello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 3,
-      lastMessageDate: "2022-10-10"
-    },
-    {
-      id: 3,
-      title: "방3",
-      content: "hello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 0,
-      lastMessageDate: "2022-10-10"
-    },
-    {
-      id: 4,
-      title: "방4",
-      content: "hello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 3,
-      lastMessageDate: "2022-10-10"
-    },
-    {
-      id: 5,
-      title: "방5",
-      content: "hello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 9999,
-      lastMessageDate: "2022-10-10"
-    },
-    {
-      id: 6,
-      title: "방5",
-      content: "hello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 9999,
-      lastMessageDate: "2022-10-10"
-    },
-    {
-      id: 7,
-      title: "방1",
-      content: "hello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 123,
-      lastMessageDate: "2022-10-10"
-    },
-    {
-      id: 8,
-      title: "방2",
-      content: "hello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 3,
-      lastMessageDate: "2022-10-10"
-    },
-    {
-      id: 9,
-      title: "방3",
-      content: "hello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 0,
-      lastMessageDate: "2022-10-10"
-    },
-    {
-      id: 10,
-      title: "방4",
-      content: "hello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 3,
-      lastMessageDate: "2022-10-10"
-    },
-    {
-      id: 11,
-      title: "방5",
-      content: "hello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 9999,
-      lastMessageDate: "2022-10-10"
-    },
-    {
-      id: 12,
-      title: "방5",
-      content: "hello world",
-      imageUrl: "https://via.placeholder.com/150",
-      unreadCount: 9999,
-      lastMessageDate: "2022-10-10"
-    }
-  ]
 
   const handleChipPress = (index: number) => {
     setActiveIndex(index);
@@ -135,15 +136,17 @@ export default function RoomListScreen({ navigation }: { navigation: NavigationP
         <View className='flex-1'>
           <ChatPaper categories={CATEGORIES} onPageChange={handleChipPress}>
             {CATEGORIES.map((category) => (
-              <>
-                <View key={category.label} className='flex items-center justify-center bg-primary h-[77px] rounded-xl'>
+              <Fragment key={`fragment_${category.id}`}>
+                <View className='flex items-center justify-center bg-primary h-[77px] rounded-xl'>
                   <Text className='text-base text-white'>더 많은 글로벌 버디와 친구가 되고 싶다면?</Text>
                 </View>
-                <View key={category.id} className="flex-1">
+                <View className="flex-1">
                   <RoomList
                     rooms={rooms}
-                    onPress={handlePressRoom} />
-                </View></>
+                    onPress={handlePressRoom}
+                  />
+                </View>
+              </Fragment>
             ))}
           </ChatPaper>
         </View>
