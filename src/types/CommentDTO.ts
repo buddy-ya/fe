477def5381373438ee7c632c7aedb979c409fe06
박현우 <gusdn3477@naver.com>
2025-01-20 16:50:12 +0900

    chore: 댓글 작성자 반환값에 포함되도록 구현

--- src/types/CommentDTO.ts
+++ src/types/CommentDTO.ts
@@ -1,35 +1,36 @@
 interface BaseDTO {
-    feedId: number;
+  feedId: number;
 }
 
 export interface CreateDTO extends BaseDTO {
-    content: string;
+  content: string;
 }
 
 export interface UpdateDTO extends BaseDTO {
-    commentId: number;
-    content: string;
+  commentId: number;
+  content: string;
 }
 
 export interface DeleteDTO extends BaseDTO {
-    commentId: number;
+  commentId: number;
 }
 
-export interface GetCommentsDTO extends BaseDTO { }
+export interface GetCommentsDTO extends BaseDTO {}
 
 // Response DTO
 export interface CommonResponse {
-    id: number;
-    content: string;
-    name: string;
-    country: string;
-    university: string;
-    profileImageUrl: string;
-    createdDate: string;
-    isFeedOwner: boolean;
-    isCommentOwner: boolean;
+  id: number;
+  userId: number;
+  content: string;
+  name: string;
+  country: string;
+  university: string;
+  profileImageUrl: string;
+  createdDate: string;
+  isFeedOwner: boolean;
+  isCommentOwner: boolean;
 }
 
 export interface UpdateResponse extends CommonResponse {
-    updatedDate: string;
+  updatedDate: string;
 }
