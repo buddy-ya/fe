fcaab3ba31561c2d621586416b7387a5e7e7a474
박현우 <gusdn3477@naver.com>
2025-01-27 16:07:10 +0900

    feat: useUserStore 완성

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,18 +1,41 @@
 import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
 
-interface UserState {
+type UserState = {
+  name: string;
+  university: string;
+  country: string;
+  gender: string | null;
+  profileImageUrl: string;
+  majors: string[];
+  languages: string[];
+  interests: string[];
   isCertificated: boolean;
   isStudentIdCardRequested: boolean;
   isKorean: boolean;
-}
+};
 
-interface UserAction {
-  update: ({ isCertificated, isStudentIdCardRequested, isKorean }: UserState) => void;
-}
+type UserAction = {
+  update: (user: Partial<UserState>) => void; // Partial로 특정 필드만 업데이트 가능
+};
 
-export const useUserStore = create<UserState & UserAction>((set) => ({
-  isCertificated: false,
-  isStudentIdCardRequested: false,
-  isKorean: false,
-  update: (user: UserState) => set((state) => ({ ...state, ...user })),
-}));
+export const useUserStore = create(
+  immer<UserState & UserAction>((set) => ({
+    name: '',
+    university: '',
+    country: 'ko',
+    gender: null,
+    profileImageUrl: '',
+    majors: [],
+    languages: [],
+    interests: [],
+    isCertificated: false,
+    isStudentIdCardRequested: false,
+    isKorean: false,
+
+    update: (user) =>
+      set((state) => {
+        Object.assign(state, user); // immer를 사용해 상태를 병합
+      }),
+  }))
+);
