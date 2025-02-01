22a458af12e6ced5a164e0b5876ca4c2a858ca5a
박현우 <gusdn3477@naver.com>
2025-02-01 15:51:46 +0900

    feat: 회원탈퇴 등 navigation reset되는 순간에 isAuthenticated 값 바꿔주는 작업

--- src/screens/verification/StudentIdCompleteScreen.tsx
+++ src/screens/verification/StudentIdCompleteScreen.tsx
@@ -1,15 +1,18 @@
 import { useTranslation } from 'react-i18next';
 import { View } from 'react-native';
 import { Button, Heading, HeadingDescription, Layout } from '@/components';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { FeedStackParamList } from '@/navigation/navigationRef';
+import { useUserStore } from '@/store';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 type EmailVerificationScreenProps = NativeStackScreenProps<FeedStackParamList, 'StudentIdComplete'>;
 
 export default function StudentIdCardCompleteScreen({ navigation }: EmailVerificationScreenProps) {
   const { t } = useTranslation('');
+  const update = useUserStore((state) => state.update);
 
   const handleNavigationButton = () => {
+    update({ isAuthenticated: true });
     navigation.reset({
       index: 0,
       routes: [{ name: 'FeedHome' }],
