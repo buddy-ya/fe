d7e7e2105c5b79e5034900f2a5e20549db6a20e2
박현우 <gusdn3477@naver.com>
2025-02-06 15:34:42 +0900

    feat: 알림 설정 페이지에서 다음 버튼 누른 경우 기존 유저의 경우 user 정보 get 해와서 업데이트 하게끔 구현

--- src/provider/AuthProvider.tsx
+++ src/provider/AuthProvider.tsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react';
 import { View, Image } from 'react-native';
-import { API, AuthRepository, UserRepository } from '@/api';
+import { API, UserRepository } from '@/api';
 import { TokenService } from '@/service';
 import { useUserStore } from '@/store';
-import * as SecureStore from 'expo-secure-store';
 import { jwtDecode, JwtPayload } from 'jwt-decode';
-import { showErrorModal, TOKEN_KEYS } from '@/utils';
 
 interface Props {
   children: React.ReactNode;
@@ -16,13 +14,11 @@ interface CustomJwtPayload extends JwtPayload {
 }
 
 export default function AuthProvider({ children }: Props) {
-  const [loading, setLoading] = useState(false);
   const [initLoading, setInitLoading] = useState(false);
-  const isAuthenticated = useUserStore((state) => state.isAuthenticated);
-  const updateUser = useUserStore((state) => state.update);
+  const update = useUserStore((state) => state.update);
 
-  const initAuth = async () => {
-    // 토큰이 있으면 API 헤더에 추가. 없으면 그냥 통과
+  // TODO: 모듈화 필요
+  const getUser = async () => {
     setInitLoading(true);
     try {
       const accessToken = await TokenService.getAccessToken();
@@ -31,59 +27,20 @@ export default function AuthProvider({ children }: Props) {
         const token: CustomJwtPayload = jwtDecode(accessToken);
         const userId = token.studentId;
         const user = await UserRepository.get({ id: userId });
-        updateUser({ ...user, isAuthenticated: true });
+        update({ ...user, isAuthenticated: true });
       }
     } catch (e) {
       console.log(e);
+    } finally {
+      setInitLoading(false);
     }
-    setInitLoading(false);
   };
 
   useEffect(() => {
-    initAuth();
-  }, [isAuthenticated]);
-
-  useEffect(() => {
-    setLoading(true);
-    API.interceptors.response.use(
-      (response) => response,
-      async (error) => {
-        if (!error.response) {
-          showErrorModal('network');
-          return Promise.reject(error);
-        }
-        const errorCode = error.response?.data?.code;
-
-        if (error.response?.status === 401 && errorCode === 3002 && !error.config._retry) {
-          error.config._retry = true;
-          try {
-            const refreshToken = await SecureStore.getItemAsync(TOKEN_KEYS.REFRESH);
-            if (!refreshToken) {
-              throw new Error('Refresh token not found');
-            }
-            const { accessToken, refreshToken: newRefreshToken } =
-              await AuthRepository.reissueToken(refreshToken);
-
-            await SecureStore.setItemAsync(TOKEN_KEYS.ACCESS, accessToken);
-            await SecureStore.setItemAsync(TOKEN_KEYS.REFRESH, newRefreshToken);
-            error.config.headers.common.Authorization = `Bearer ${accessToken}`;
-            return API(error.config);
-          } catch (reissueError) {
-            await SecureStore.deleteItemAsync(TOKEN_KEYS.ACCESS);
-            await SecureStore.deleteItemAsync(TOKEN_KEYS.REFRESH);
-            delete API.defaults.headers.common['Authorization'];
-            updateUser({ isAuthenticated: false });
-            showErrorModal('tokenExpired');
-            return Promise.reject(reissueError);
-          }
-        }
-        return Promise.reject(error);
-      }
-    );
-    setLoading(false);
+    getUser();
   }, []);
 
-  if (initLoading || loading)
+  if (initLoading)
     return (
       <View className="flex-1 items-center justify-center">
         <Image source={require('@assets/images/splash.png')} className="h-full w-full" />
