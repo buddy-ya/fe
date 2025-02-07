1e8a6a6b9628de6de32314d8943c1edd875950ed
박현우 <gusdn3477@naver.com>
2025-02-08 00:45:48 +0900

    feat: ChatRequest country 타입 고도화

--- src/types/ChatDTO.ts
+++ src/types/ChatDTO.ts
@@ -1,9 +1,13 @@
+import { COUNTRY_IDS } from '@/utils';
+
+type CountryId = (typeof COUNTRY_IDS)[number];
+
 export interface ChatRequest {
   id: number;
   senderId: number;
   university: string;
   name: string;
-  country: string;
+  country: CountryId;
   profileImageUrl: string;
   createdDate: string;
 }
