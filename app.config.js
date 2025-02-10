ff52181254ccb6fe7e7029ed4a209914e834adbe
박현우 <46596758+gusdn3477@users.noreply.github.com>
2025-02-11 00:47:47 +0900

    chore: app version up(1.0.2)

--- app.config.js
+++ app.config.js
@@ -9,7 +9,7 @@ module.exports = {
     owner: 'buddyya',
     description: 'A friendly app for buddy management.',
     slug: 'buddyya',
-    version: '1.0.1',
+    version: '1.0.2',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
     newArchEnabled: false,
