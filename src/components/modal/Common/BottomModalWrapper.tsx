7c533d7febea51e2135115720d5fe546080521f3
mangsuyo <mangsuyo3610@gmail.com>
2025-02-25 18:50:28 +0900

    feat: 모달 애니메이션 구현 (#59)

--- src/components/modal/Common/BottomModalWrapper.tsx
+++ src/components/modal/Common/BottomModalWrapper.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Modal, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Modal, TouchableOpacity, View, Animated } from 'react-native';
 import MyText from '@/components/common/MyText';
 
 interface BottomModalWrapperProps {
@@ -23,43 +23,65 @@ export function BottomModalWrapper({
   confirmText,
   onConfirm,
 }: BottomModalWrapperProps) {
+  // 초기값 300: 모달 콘텐츠가 화면 아래쪽에서 시작
+  const slideAnim = useRef(new Animated.Value(300)).current;
+
+  useEffect(() => {
+    if (visible) {
+      // visible 상태가 true가 될 때마다 초기 위치를 재설정
+      slideAnim.setValue(300);
+      Animated.spring(slideAnim, {
+        toValue: 0,
+        useNativeDriver: true,
+        damping: 100,
+        stiffness: 200,
+        overshootClamping: true, // 약간의 오버슈팅(넘침) 방지
+      }).start();
+    }
+  }, [visible, slideAnim]);
+
   return (
-    <Modal visible={visible} transparent animationType="slide" onRequestClose={onClose}>
+    <Modal visible={visible} transparent animationType="none" onRequestClose={onClose}>
       <TouchableOpacity className="flex-1 bg-black/50" activeOpacity={1} onPress={onClose}>
-        <View className="absolute bottom-0 w-full rounded-[12px] bg-white">
-          <View className="p-5">
-            <View>
-              <MyText size="text-[20px]" className="font-semibold">
-                {title}
-              </MyText>
-              <MyText size="text-[14px] mt-4" color="text-textDescription">
-                {description}
-              </MyText>
-            </View>
-            {children}
-            <View className="mb-2 mt-10 flex-row gap-2">
-              <TouchableOpacity
-                className="mr-3 flex-1 justify-center rounded-[12px] bg-[#E8E9EB] py-[12px]"
-                onPress={onClose}
-              >
-                <MyText size="text-[16px]" className="text-center font-semibold text-white">
-                  {cancelText}
+        <Animated.View
+          style={{ transform: [{ translateY: slideAnim }] }}
+          className="absolute bottom-0 w-full"
+        >
+          <View className="rounded-[12px] bg-white">
+            <View className="p-5">
+              <View>
+                <MyText size="text-[20px]" className="font-semibold">
+                  {title}
                 </MyText>
-              </TouchableOpacity>
-              <TouchableOpacity
-                className="flex-1 justify-center rounded-[12px] bg-primary"
-                onPress={() => {
-                  onConfirm();
-                  onClose();
-                }}
-              >
-                <MyText size="text-[16px]" className="text-center font-semibold text-white">
-                  {confirmText}
+                <MyText size="text-[14px] mt-4" color="text-textDescription">
+                  {description}
                 </MyText>
-              </TouchableOpacity>
+              </View>
+              {children}
+              <View className="mb-2 mt-10 flex-row gap-2">
+                <TouchableOpacity
+                  className="mr-3 flex-1 justify-center rounded-[12px] bg-[#E8E9EB] py-[12px]"
+                  onPress={onClose}
+                >
+                  <MyText size="text-[16px]" className="text-center font-semibold text-white">
+                    {cancelText}
+                  </MyText>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  className="flex-1 justify-center rounded-[12px] bg-primary"
+                  onPress={() => {
+                    onConfirm();
+                    onClose();
+                  }}
+                >
+                  <MyText size="text-[16px]" className="text-center font-semibold text-white">
+                    {confirmText}
+                  </MyText>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-        </View>
+        </Animated.View>
       </TouchableOpacity>
     </Modal>
   );
