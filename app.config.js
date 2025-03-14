74ee97ee2095f723c660c08713571534d7f14613
mangsuyo <mangsuyo3610@gmail.com>
2025-03-14 17:30:01 +0900

    deploy: ios v1.0.5 배포 (#69)

--- app.config.js
+++ app.config.js
@@ -9,7 +9,7 @@ module.exports = {
     owner: 'buddyya',
     description: 'A friendly app for buddy management.',
     slug: 'buddyya',
-    version: '1.0.4',
+    version: '1.0.5',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
     newArchEnabled: false,
