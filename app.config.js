e9bba9f3e91c3e55cb4b4f2a40ff8baa652b6a95
mangsuyo <mangsuyo3610@gmail.com>
2025-03-18 15:19:41 +0900

    chore: projectId 변경 (#61)

--- app.config.js
+++ app.config.js
@@ -59,7 +59,7 @@ module.exports = {
       BASE_URL: process.env.BASE_URL,
       BASE_DOMAIN: process.env.BASE_DOMAIN,
       eas: {
-        projectId: 'ae3e9ea5-3d73-4892-894d-2308ea11ed5c',
+        projectId: 'ac2fa962-0fb3-4186-9c25-840a607c0c90',
       },
     },
   },
