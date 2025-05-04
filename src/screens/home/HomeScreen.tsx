a453861add8da38657b38f80d982a030109ed117
mangsuyo <mangsuyo3610@gmail.com>
2025-05-05 04:47:01 +0900

    feat: 피드 카테고리 수정 (#94)

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -25,7 +25,7 @@ export function HomeScreen({ navigation }: FeedHomeScreenProps) {
 
   const tab = selectedTab === 'myUni' ? userUniversity : 'all';
 
-  const categoriesToShow = tab === 'all' ? CATEGORIES : CATEGORIES;
+  const categoriesToShow = tab === 'all' ? [CATEGORIES[0], CATEGORIES[2]] : [CATEGORIES[0]];
 
   const [activeCategory, setActiveCategory] = useState(categoriesToShow[0].id);
 
