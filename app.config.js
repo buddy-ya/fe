18e4238bb4da9acdb49e966a1ef5ac336fb78ff6
mangsuyo <mangsuyo3610@gmail.com>
2025-05-14 11:13:05 +0900

    deploy: v1.1.5 (#92)

--- app.config.js
+++ app.config.js
@@ -10,7 +10,7 @@ module.exports = {
     slug: 'buddyya',
     scheme: 'buddyya',
     owner: 'buddyya',
-    version: '1.1.4',
+    version: '1.1.5',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
     description: 'A friendly app for buddy management.',
